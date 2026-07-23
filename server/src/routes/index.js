360beac5aae0d4434433e8aa51c98d77f2eb2893
sarim-alis <sarimslayerali786@gmail.com>
2026-07-22 18:02:15 -0700

    featL auth routes

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const authRoutes = require("./authRoutes");
 
 const router = express.Router();
 
 router.get("/health", (_req, res) => { res.json({ message: "API is healthy 🍋", status: "running" })});
+router.use("/auth", authRoutes);
 
 module.exports = router;
