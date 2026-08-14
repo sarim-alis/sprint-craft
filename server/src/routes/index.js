8ec63550bb34e8a42a59fd2ff4715884a650fe76
sarim-alis <sarimslayerali786@gmail.com>
2026-08-14 16:56:56 -0700

    feat: user and board routes

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const authRoutes = require("./authRoutes");
+const boardRoutes = require("./boardRoutes");
+const userRoutes = require("./userRoutes");
 
 const router = express.Router();
 
 router.get("/health", (_req, res) => { res.json({ message: "API is healthy 🍋", status: "running" })});
 router.use("/auth", authRoutes);
+router.use("/boards", boardRoutes);
+router.use("/users", userRoutes);
 
 module.exports = router;
